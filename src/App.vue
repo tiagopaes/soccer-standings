1503bd3283d52ecea1bb491b2e7c5c621ce2cf61
tiagopaes <tiagopaes37@gmail.com>
2019-10-09 18:16:14 -0300

    Add github button

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
       <v-toolbar-title
         @click="$route.name !== 'home' ? $router.push({ name: 'home'}) : ''"
       >Soccer Standings</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <!-- Place this tag where you want the button to render. -->
+      <a
+        class="github-button"
+        href="https://github.com/tiagopaes/soccer-standings"
+        data-size="large"
+        data-show-count="true"
+        aria-label="Star tiagopaes/soccer-standings on GitHub"
+      >Star</a>
     </v-app-bar>
 
     <v-content>
