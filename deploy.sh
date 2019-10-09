34a1ce26f6f1edb9d6a3bca6abed6a4efffdc5f3
tiagopaes <tiagopaes37@gmail.com>
2019-10-09 17:06:04 -0300

    create deploy files

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+# sed -i 's/\/static/.\/static/g' index.html
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://tiagopaes.github.io/to-do-list
+git push -f git@github.com:tiagopaes/soccer-standings.git master:gh-pages
+
+cd -
