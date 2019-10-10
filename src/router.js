0cfe42e16c1a849715bcf7858a02727715d093d1
Tiago Paes <tiagopaes37@gmail.com>
2019-10-09 22:55:06 -0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ import { competitions } from "./constants/index";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: "hash",
   base: process.env.VUE_APP_BASE_URL,
   routes: [
     {
